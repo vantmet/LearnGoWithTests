f64c0f5369585f35c19bd13715f0db88a0cde5e8
Anthony Metcalf <anthony.metcalf1@gmail.com>
2021-06-01 10:58:17 +0100

    More fun with structs

--- Go Fundamentals/05-Structs/shapes.go
+++ Go Fundamentals/05-Structs/shapes.go
@@ -11,6 +11,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 type Shape interface {
 	Area() float64
 }
@@ -21,6 +26,10 @@ func (r Rectangle) Area() float64 {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
 }
 
 func Perimeter(rectangle Rectangle) float64 {
--- Go Fundamentals/05-Structs/shapes_test.go
+++ Go Fundamentals/05-Structs/shapes_test.go
@@ -2,14 +2,6 @@ package main
 
 import "testing"
 
-func checkArea(t testing.TB, shape Shape, want float64) {
-	t.Helper()
-	got := shape.Area()
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
-}
-
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
 	got := Perimeter(rectangle)
@@ -20,13 +12,19 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		checkArea(t, rectangle, 72.0)
-	})
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+	}
 
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("%#v has Area %g want %g", tt.shape, got, tt.want)
+		}
+	}
 }
