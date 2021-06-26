8678bd8adf02a0a2a3c96e712959a83a2123018f
Anthony Metcalf <anthony.metcalf1@gmail.com>
2021-06-26 12:19:01 +0100

    Working for 4.

--- GoFundamentals/15-PropBasedTests/numeral.go
+++ GoFundamentals/15-PropBasedTests/numeral.go
@@ -5,7 +5,11 @@ import "strings"
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
-	for i := 0; i < arabic; i++ {
+	for i := arabic; i > 0; i-- {
+		if arabic == 4 {
+			result.WriteString("IV")
+			break
+		}
 		result.WriteString("I")
 	}
 
