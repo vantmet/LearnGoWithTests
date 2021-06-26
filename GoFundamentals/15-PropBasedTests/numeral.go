6b628aa52944938dc2b2befe09e5233235089736
Anthony Metcalf <anthony.metcalf1@gmail.com>
2021-06-26 18:44:52 +0100

    Now converting back to Roman

--- GoFundamentals/15-PropBasedTests/numeral.go
+++ GoFundamentals/15-PropBasedTests/numeral.go
@@ -7,7 +7,9 @@ type RomanNumeral struct {
 	Symbol string
 }
 
-var allRomanNumerals = []RomanNumeral{
+type RomanNumerals []RomanNumeral
+
+var allRomanNumerals = RomanNumerals{
 	{1000, "M"},
 	{900, "CM"},
 	{500, "D"},
@@ -23,6 +25,16 @@ var allRomanNumerals = []RomanNumeral{
 	{1, "I"},
 }
 
+func (r RomanNumerals) ValueOf(symbol string) int {
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return s.Value
+		}
+	}
+
+	return 0
+}
+
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
@@ -34,4 +46,33 @@ func ConvertToRoman(arabic int) string {
 	}
 
 	return result.String()
+}
+
+func ConvertToArabic(roman string) int {
+	total := 0
+
+	for i := 0; i < len(roman); i++ {
+		symbol := roman[i]
+
+		// look ahead to next symbol if we can and, the current symbol is base 10 (only valid subtractors)
+		if i+1 < len(roman) && symbol == 'I' {
+			nextSymbol := roman[i+1]
+
+			// build the two character string
+			potentialNumber := string([]byte{symbol, nextSymbol})
+
+			// get the value of the two character string
+			value := allRomanNumerals.ValueOf(potentialNumber)
+
+			if value != 0 {
+				total += value
+				i++ // move past this character too for the next loop
+			} else {
+				total++
+			}
+		} else {
+			total++
+		}
+	}
+	return total
 }
