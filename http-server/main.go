e6d9028d62adcb2a56bed3c93234a42f2979ceed
Anthony Metcalf <anthony.metcalf1@gmail.com>
2021-07-03 12:41:36 +0100

    Implement and in memory player store

--- http-server/main.go
+++ http-server/main.go
@@ -5,18 +5,7 @@ import (
 	"net/http"
 )
 
-//Hardcode in memory store for now
-type InMemoryPlayerStore struct{}
-
-//Hardcode in memory store for now
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
-}
-
-// Record a win.
-func (i *InMemoryPlayerStore) RecordWin(name string) {}
-
 func main() {
-	server := &PlayerServer{&InMemoryPlayerStore{}}
+	server := &PlayerServer{NewInMemoryPlayerStore()}
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
