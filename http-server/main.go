5992651a18e8801fd2565ed4023a9dc0c689ff95
Anthony Metcalf <anthony.metcalf1@gmail.com>
2021-07-03 11:28:47 +0100

    "Working" application now returns a value

--- http-server/main.go
+++ http-server/main.go
@@ -5,7 +5,15 @@ import (
 	"net/http"
 )
 
+//Hardcode in memory store for now
+type InMemoryPlayerStore struct{}
+
+//Hardcode in memory store for now
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
-	handler := http.HandlerFunc(PlayerServer)
-	log.Fatal(http.ListenAndServe(":5000", handler))
+	server := &PlayerServer{&InMemoryPlayerStore{}}
+	log.Fatal(http.ListenAndServe(":5000", server))
 }
--- http-server/server.go
+++ http-server/server.go
@@ -10,11 +10,13 @@ type PlayerStore interface {
 	GetPlayerScore(name string) int
 }
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
-
-	fmt.Fprint(w, GetPlayerScore(player))
-
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
 }
 
 func GetPlayerScore(name string) string {
