a59f0cb475284c66f87af8c810b1d6a7d54055e9
Anthony Metcalf <anthony.metcalf1@gmail.com>
2021-07-30 20:34:05 +0100

    Ensure there is valid JSON in the integration test

--- http-server/server_integration_test.go
+++ http-server/server_integration_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestRecordingWinsAndRetrievingThem(t *testing.T) {
-	database, cleanDatabase := createTempFile(t, "")
+	database, cleanDatabase := createTempFile(t, `[]`)
 	defer cleanDatabase()
 	store, err := NewFileSystemPlayerStore(&database)
 	assertNoError(t, err)
