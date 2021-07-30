990ef6d20df105eac84e371193215429dfe1ddb2
Anthony Metcalf <anthony.metcalf1@gmail.com>
2021-07-30 22:44:09 +0100

    Refactor common code to function

--- http-server/cmd/webserver/main.go
+++ http-server/cmd/webserver/main.go
@@ -3,25 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
-  "github.com/vantmet/LearnGoWithTests/http-server"
+	poker "github.com/vantmet/LearnGoWithTests/http-server"
 )
 
 const dbFileName = "game.db.json"
 
 func main() {
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 
 	if err != nil {
-		log.Fatalf("problem opening %s %v", dbFileName, err)
+		log.Fatal(err)
 	}
+	defer close()
 
-	store, err := poker.NewFileSystemPlayerStore(db)
-
-	if err != nil {
-		log.Fatalf("problem creating file system player store, %v", err)
-	}
 	server := poker.NewPlayerServer(store)
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
