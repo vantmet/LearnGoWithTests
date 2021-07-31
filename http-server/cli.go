04f9d533ab0aed6f8f0f5e56aec8b9536871bc0d
Anthony Metcalf <anthony.metcalf1@gmail.com>
2021-07-31 19:58:24 +0100

    Add failing test for blind alerter

--- http-server/cli.go
+++ http-server/cli.go
@@ -4,14 +4,19 @@ import (
 	"bufio"
 	"io"
 	"strings"
+	"time"
 )
+
+type BlindAlerter interface {
+	ScheduleAlertAt(duration time.Duration, amount int)
+}
 
 type CLI struct {
 	playerstore PlayerStore
 	in          *bufio.Scanner
 }
 
-func NewCLI(store PlayerStore, in io.Reader) *CLI {
+func NewCLI(store PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
 	return &CLI{playerstore: store,
 		in: bufio.NewScanner(in),
 	}
--- http-server/cli_test.go
+++ http-server/cli_test.go
@@ -3,16 +3,19 @@ package poker_test
 import (
 	"strings"
 	"testing"
+	"time"
 
 	poker "github.com/vantmet/LearnGoWithTests/http-server"
 )
+
+var dummySpyAlerter = &SpyBlindAlerter{}
 
 func TestCLI(t *testing.T) {
 	t.Run("record chris win from user input", func(t *testing.T) {
 		playerStore := &poker.StubPlayerStore{}
 		input := strings.NewReader("Chris wins\n")
 
-		cli := poker.NewCLI(playerStore, input)
+		cli := poker.NewCLI(playerStore, input, dummySpyAlerter)
 		cli.PlayPoker()
 
 		poker.AssertPlayerWin(t, playerStore, "Chris")
@@ -21,10 +24,36 @@ func TestCLI(t *testing.T) {
 		playerStore := &poker.StubPlayerStore{}
 		input := strings.NewReader("Cleo wins\n")
 
-		cli := poker.NewCLI(playerStore, input)
+		cli := poker.NewCLI(playerStore, input, dummySpyAlerter)
 		cli.PlayPoker()
 
 		poker.AssertPlayerWin(t, playerStore, "Cleo")
 	})
+	t.Run("it schedules printing of blind values", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &poker.StubPlayerStore{}
+		blindAlerter := &SpyBlindAlerter{}
 
+		cli := poker.NewCLI(playerStore, in, blindAlerter)
+		cli.PlayPoker()
+
+		if len(blindAlerter.alerts) != 1 {
+			t.Fatal("expected a blind alert to be scheduled")
+		}
+	})
+
+}
+
+type SpyBlindAlerter struct {
+	alerts []struct {
+		scheduledAt time.Duration
+		amount      int
+	}
+}
+
+func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
+	s.alerts = append(s.alerts, struct {
+		scheduledAt time.Duration
+		amount      int
+	}{duration, amount})
 }
