8122d4727c681b4a312fc963e3dfc5c594002825
Anthony Metcalf <anthony.metcalf1@gmail.com>
2021-07-31 20:08:26 +0100

    create blinds properly.

--- http-server/cli.go
+++ http-server/cli.go
@@ -14,15 +14,25 @@ type BlindAlerter interface {
 type CLI struct {
 	playerstore PlayerStore
 	in          *bufio.Scanner
+	alerter     BlindAlerter
 }
 
 func NewCLI(store PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
-	return &CLI{playerstore: store,
-		in: bufio.NewScanner(in),
+	return &CLI{
+		playerstore: store,
+		in:          bufio.NewScanner(in),
+		alerter:     alerter,
 	}
 }
 
 func (cli *CLI) PlayPoker() {
+	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
+	blindTime := 0 * time.Second
+	for _, blind := range blinds {
+		cli.alerter.ScheduleAlertAt(blindTime, blind)
+		blindTime = blindTime + 10*time.Minute
+	}
+
 	userInput := cli.readLine()
 	cli.playerstore.RecordWin(extractWinner(userInput))
 }
